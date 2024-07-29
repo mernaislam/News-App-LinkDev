91ae0bb12a3865e17a440c24fb20492e39ace6b2
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-07-29 15:21:32 +0300

    feat: Add pretty dio logger package

--- news_app/lib/service/news_service.dart
+++ news_app/lib/service/news_service.dart
@@ -1,11 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:news_app/model/article_model.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class NewsService {
   final Dio dio;
   final String apiKey = '1c0f731cca954a13875e6965f9c7e9de';
 
-  const NewsService({required this.dio});
+  NewsService({required this.dio}){
+    dio.interceptors.add(PrettyDioLogger());
+  }
 
   Future<List<ArticleModel>> getNews() async {
     try {
